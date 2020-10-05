786094ce9b890646df85cb1a0d6ed12d188ef510
Natee159 <s6004062630248@email.kmutnb.ac.th>
2020-10-05 11:29:30 +0700

    add allbook

--- src/page/allbook.js
+++ src/page/allbook.js
@@ -4,21 +4,17 @@ import { Container, Row, Col } from 'reactstrap';
 import Book from '../component/book.js';
 import Slide from '../component/slide.js';
 import axios from 'axios';
-import { useHistory } from "react-router-dom";
-const Home = () => {
+
+const Allbook = () => {
   const [data, setData] = useState([])
-  const history = useHistory();
+
   useEffect(() => {
-    axios.get(`http://localhost/api/product/show.php`)
+    axios.get(`http://localhost/api/product/read.php`)
       .then(res => {
         console.log(res.data.records)
         setData(res.data.records)
       })
   }, []);
-
-  const handleClick = () => {
-    history.push("/allbook");
-}
 
   return (
       <Container>
@@ -29,17 +25,7 @@ const Home = () => {
             <Row>
               {data.map(d => <Book data={d} />)}
             </Row>
-            <p onClick={handleClick} style={{ textAlign: 'right' }}>ดูรายการสินค้าเพิ่มเติม</p>
-            <h1>หนังสือขายดี</h1>
-            <Row>
-              {data.map(d => <Book data={d} />)}
-            </Row>
-            <p style={{ textAlign: 'right' }}>ดูรายการสินค้าเพิ่มเติม</p>
-            <h1>หนังสือออกใหม่</h1>
-            <Row>
-              {data.map(d => <Book data={d} />)}
-            </Row>
-            <p style={{ textAlign: 'right' }}>ดูรายการสินค้าเพิ่มเติม</p>
+
           </Col>
           <Col className="picRight" xs="hide" sm="2" >
             <img width={'100%'} src={require('../component/pic/b4.PNG')} alt="picRight"/>
@@ -58,4 +44,4 @@ const Home = () => {
   );
 }
 
-export default Home;
+export default Allbook;
